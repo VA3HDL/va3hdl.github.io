6e20e0e6eec36cd4a91c9724033b5539cb84ac24
VA3HDL <va3hdl@gmail.com>
2024-06-11 21:37:11 -0400

    Fixed issue with dated video

--- hamdash/config.js
+++ hamdash/config.js
@@ -57,6 +57,7 @@ const aURL = [
 // You can't add more items because there are only 12 placeholders on the dashboard
 // but you can replace the titles and the images with anything you want.
 const currentDate = new Date();
+currentDate.setDate(currentDate.getDate() - 1);
 const aIMG = [
   ["RADAR", "https://radar.weather.gov/ridge/standard/CONUS_loop.gif"],
   [
